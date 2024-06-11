6049e039b4efd4cc9d8b57b38d11a1a62c6ff447
ulushbey <ulushbey.h@gmail.com>
2024-06-11 18:19:05 +0900

    fix: modify todolist page

--- src/app/components/TodoFilter/index.tsx
+++ src/app/components/TodoFilter/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TodoFilter = () => {
-  return <div>TodoFilter</div>;
+  return (
+    <div className="flex justify-evenly gap-5 my-5">
+      <button className="bg-green-500 w-full p-2 rounded-lg hover:bg-green-600 text-white">
+        ALL
+      </button>
+      <button className="bg-green-500 w-full p-2 rounded-lg hover:bg-green-600 text-white">
+        DONE
+      </button>
+      <button className="bg-green-500 w-full p-2 rounded-lg hover:bg-green-600 text-white">
+        TODO
+      </button>
+    </div>
+  );
 };
 
 export default TodoFilter;
--- src/app/components/TodoList/index.tsx
+++ src/app/components/TodoList/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Todo } from "../types/Todo.type";
+import TodoFilter from "../TodoFilter";
 
 type T = {
   todos: Todo[];
 };
 const TodoList = ({ todos }: T) => {
-  return <div>TodoList</div>;
+  return (
+    <div>
+      <h3 className="text-3xl text-center mt-5">TodoList</h3>
+      <TodoFilter />
+    </div>
+  );
 };
 
 export default TodoList;
